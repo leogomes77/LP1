e37a31467c218f19e01d4630c8bbe98b316341ac
leogomes77 <77128004+leogomes77@users.noreply.github.com>
2021-01-10 21:19:33 +0000

    Leo

--- Supermercado/Supermercado/ListaUsers.cs
+++ Supermercado/Supermercado/ListaUsers.cs
@@ -24,10 +24,11 @@ namespace Supermercado
             string localizacaoDoFicheiro = Directory.GetCurrentDirectory();
             string nomeDoFicheiro = "usernames.txt";
 
-            //Validaçao
+
+            //Validação
             if (File.Exists(nomeDoFicheiro))
             {
-                
+                File.Delete(nomeDoFicheiro);
             }
 
             FileStream fileStream = File.Create(nomeDoFicheiro);
@@ -69,12 +70,14 @@ namespace Supermercado
         public void Login()
         {
             Console.Clear();
+            Console.WriteLine("---------- LOGIN ----------");
             Console.Write("\t Username  : ");
             string username = Console.ReadLine();
             Console.Write("\t Password  : ");
             string pw = Console.ReadLine();
             Console.WriteLine("\n");
             Console.Clear();
+            string cargo = "";
             
             
             bool encontrou = false;
@@ -83,6 +86,7 @@ namespace Supermercado
                 if(String.Compare(username, user.username) == 0 && String.Compare(pw, user.password) == 0)
                 {
                     encontrou = true;
+                    cargo = user.cargo;
                 }
             }
 
@@ -90,12 +94,157 @@ namespace Supermercado
             {
                 Console.Clear();
                 Console.WriteLine("Logado");
+                MenuPrincipal(username, cargo);
             }
             else
             {
                 Console.Clear();
                 Console.WriteLine("Não existe");
             }
+        }
+
+        public void RegistarFuncionario()
+        {
+            Console.Clear();
+            Console.WriteLine("---------- REGISTAR FUNCIONARIO ----------");
+            Console.Write("\t Username  : ");
+            string usernameNovo = Console.ReadLine();
+            Console.Write("\t Password  : ");
+            string pwNova = Console.ReadLine();
+            Console.Write("\t Cargo  : ");
+            string cargoNovo = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.Clear();
+
+            leituraUsers();
+
+            bool existe = false;
+            //Ver se já existe
+            foreach(User user in listaUsers)
+            {
+                if (String.Compare(usernameNovo, user.username) == 0)
+                {
+                    existe = true;
+                }
+                
+            }
+
+            if (existe == true)
+            {
+                Console.Clear();
+                Console.WriteLine("Já existe");
+                Console.ReadKey();
+                Login();
+            }
+
+            else
+            {
+                User novoUser = new User(usernameNovo, pwNova, cargoNovo);
+                listaUsers.Add(novoUser);
+                SaveUsers();
+                Console.Clear();
+                Console.WriteLine("Registo Efetuado");
+                Login();
+            }
+        }
+
+        public void MenuPrincipal(string username, string cargo)
+        {
+            Console.Clear();
+            
+            Console.WriteLine("---------- Bom dia, {0} ----------", username);
+            string resposta = "";
+           
+            
+            
+            if (String.Compare("gerente", cargo, true) == 0)
+            {
+                Console.WriteLine("1 - Registar Funcionário");
+                Console.WriteLine("2 - Apagar Funcionário");
+                Console.WriteLine("3 - Vender");
+                resposta = Console.ReadLine();
+
+                switch (resposta)
+                {
+                    case "1":
+                        Console.Clear();
+                        RegistarFuncionario();
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Username do Funcionario a apagar: ");
+                        string funcionario = Console.ReadLine();
+                        int apagou = ApagarFuncionario(funcionario);
+                        if (apagou == 1) {
+                            Console.Clear();
+                            Console.WriteLine("Apagado com Sucesso");
+                            Console.ReadKey();
+                            MenuPrincipal(username,cargo);
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("User não existe");
+                            Console.ReadKey();
+                            MenuPrincipal(username, cargo);
+                        }
+                        break;
+
+                    case "3":
+                        Console.Clear();
+                        //Vender;
+                        break;
+
+                    default:
+                        Console.WriteLine("Não é possivel");
+                        MenuPrincipal(username, cargo);
+                        break;
+                }
+
+                
+            }
+            if (String.Compare("repositor", cargo, true) == 0)
+            {
+                Console.WriteLine("1 - Adicionar Novo Produto");
+               
+                resposta = Console.ReadLine();
+                switch (resposta)
+                {
+                    case "1":
+                        Console.Clear();
+                        //Adicionar Produto
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        //Vender();
+                        break;
+
+                    default:
+                        Console.WriteLine("Não é possivel");
+                        MenuPrincipal(username,cargo);
+                        break;
+                }
+
+
+            }
+        }
+
+
+        public int ApagarFuncionario(string funcionario)
+        {
+            foreach(User user in listaUsers)
+            {
+                if (String.Compare(user.username, funcionario) == 0)
+                {
+                    listaUsers.Remove(user);
+                    SaveUsers();
+                    return 1;
+                }
+            }
+            
+            return 0;
         }
     }
 }
--- Supermercado/Supermercado/Program.cs
+++ Supermercado/Supermercado/Program.cs
@@ -13,46 +13,8 @@ namespace Supermercado
 
             ListaUsers listaUtilizadores = new ListaUsers();
 
-            //User user1 = new User("teste1", "teste1", "Gerente");
-            //User user2 = new User("teste2", "teste2", "Funcionario");
-
-
-
-
             listaUtilizadores.leituraUsers();
-
-            int resposta;
-            Console.WriteLine("Escolha a opção:\n");
-            Console.WriteLine("1 - Login");
-            Console.WriteLine("2 - Registar");
-            resposta = int.Parse(Console.ReadLine());
-
-            
-            switch(resposta){
-                case 1:
-                    listaUtilizadores.Login();
-                    break;
-
-                case 2:
-
-                    Console.Clear();
-                    Console.Write("\t Username  : ");
-                    string user1 = Console.ReadLine();
-                    Console.Write("\t Password  : ");
-                    string pw1 = Console.ReadLine();
-                    Console.WriteLine("\n");
-                    break;
-
-                default:
-                    Console.WriteLine("Não é possivel");
-                    break;
-            }
-
-
-            
-
-        
-
+            listaUtilizadores.Login();
 
            Console.ReadKey();
             
